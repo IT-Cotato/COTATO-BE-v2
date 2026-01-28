97c8fbf3f02f13840135cc5339e7a7cd1ee269f1
jeongchanmin <jjw421412@naver.com>
2026-01-28 10:11:14 +0900

    feat: GenerationMember에 position 필드 추가

--- src/main/java/org/cotato/homepage/domain/generation/repository/GenerationMemberRepository.java
+++ src/main/java/org/cotato/homepage/domain/generation/repository/GenerationMemberRepository.java
@@ -1,10 +1,13 @@
 package org.cotato.homepage.domain.generation.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.cotato.homepage.domain.auth.entity.Member;
 import org.cotato.homepage.domain.generation.entity.Generation;
 import org.cotato.homepage.domain.generation.entity.GenerationMember;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -29,4 +32,18 @@ public interface GenerationMemberRepository extends JpaRepository<GenerationMemb
 	boolean existsByGenerationAndMember(Generation generation, Member member);
 
 	boolean existsByGenerationAndMemberIn(Generation generation, List<Member> member);
+
+	@Transactional
+	@Modifying
+	@Query("delete from GenerationMember gm where gm.member in :members")
+	void deleteAllByMemberIn(@Param("members") List<Member> members);
+
+	@Transactional(readOnly = true)
+	@Query("select gm from GenerationMember gm join fetch gm.member m where gm.generation.id = :generationId")
+	Slice<GenerationMember> findAllByGenerationIdWithMemberSlice(
+		@Param("generationId") Long generationId, Pageable pageable);
+
+	Optional<GenerationMember> findByGenerationAndMember(Generation generation, Member member);
+
+	boolean existsByMemberAndIdNot(Member member, Long excludeId);
 }
