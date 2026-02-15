4e9d8600fa38abbf9edb6429fa25751b51069f40
jeongchanmin <jjw421412@naver.com>
2026-02-15 14:23:25 +0900

    feat: 모집 활성화 및 토글 api 추가

---
+++ src/main/java/org/cotato/homepage/api/recruit/controller/AdminRecruitmentController.java
@@ -0,0 +1,30 @@
+package org.cotato.homepage.api.recruit.controller;
+
+import org.cotato.homepage.common.role.RoleAuthority;
+import org.cotato.homepage.domain.member.enums.MemberRole;
+import org.cotato.homepage.domain.recruit.service.RecruitmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "관리자 모집 관리", description = "관리자 모집 관리 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/api/admin/recruitments")
+public class AdminRecruitmentController {
+
+	private final RecruitmentService recruitmentService;
+
+	@Operation(summary = "모집 상태 토글 API")
+	@RoleAuthority(MemberRole.OPERATION)
+	@PatchMapping("/toggle")
+	public ResponseEntity<Void> toggleRecruitmentStatus() {
+		recruitmentService.toggleRecruitmentStatus();
+		return ResponseEntity.noContent().build();
+	}
+}
