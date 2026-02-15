2327859afb4e5fef9fa8677a6360479343437608
jeongchanmin <jjw421412@naver.com>
2026-02-15 13:02:30 +0900

    feat: cors 허용 api 수정

--- src/main/java/org/cotato/homepage/common/config/CorsConfig.java
+++ src/main/java/org/cotato/homepage/common/config/CorsConfig.java
@@ -30,6 +30,8 @@ public class CorsConfig {
 
 		config.addAllowedOrigin("http://localhost:3000");
 		config.addAllowedOrigin("http://localhost:3001");
+		config.addAllowedOrigin("https://cotato-fe-v2-homepage-git-develop-kimminas-projects.vercel.app");
+		config.addAllowedOrigin("https://cotato-fe-v2-homepage.vercel.app");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("*");
 		config.setExposedHeaders(List.of("accessToken", HttpHeaders.CONTENT_DISPOSITION, "Set-Cookie"));
