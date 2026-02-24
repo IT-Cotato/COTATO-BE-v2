56b2bf1cf87434daf5a0e377479eadbdccbc0c4f
jeongchanmin <jjw421412@naver.com>
2026-02-25 01:00:51 +0900

    refactor: 코드 리팩토링

--- src/main/java/org/cotato/homepage/domain/attendance/enums/AttendanceOpenStatus.java
+++ src/main/java/org/cotato/homepage/domain/attendance/enums/AttendanceOpenStatus.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AttendanceOpenStatus {
-	CLOSED("출결 입력 기간이 마감되었습니다."),
+	CLOSED("출결 입력 기간이 마감되었습니다"),
 	OPEN("현재 출석 진행 중"),
 	LATE("현재 출결 입력 시 지각"),
 	ABSENT("현재 출결 입력 시 결석"),
